f3ef217bae091955fcccbc2ebcadc9279c0d6ec7
Lukasz Majewski <lukma@denx.de>
2023-02-10 16:32:59 +0100

    demo: x4-square: Update x4 test program to generate 1ms interrupts
    Signed-off-by: Lukasz Majewski <lukma@denx.de>

--- recipes-demo/x4-sqw/x4-sqw/x4_evl_square_wave.c
+++ recipes-demo/x4-sqw/x4-sqw/x4_evl_square_wave.c
@@ -11,11 +11,15 @@
 #include <unistd.h>
 #include <error.h>
 #include <string.h>
+#include <time.h>
 #include <sys/ioctl.h>
 #include <linux/gpio.h>
 #include <uapi/evl/devices/gpio.h>
 #include <evl/evl.h>
 #include <evl/sched.h>
+#include <evl/timer.h>
+#include <evl/clock.h>
+#include <evl/proxy.h>
 
 static int gpio_outfd = -1;
 
@@ -38,12 +42,30 @@ static int setup_gpio_out_pin(int fd, unsigned char gn) {
 	return out.fd;
 }
 
+void timespec_add_ns(struct timespec *__restrict r,
+	     		     const struct timespec *__restrict t,
+			     long ns) {
+	long s, rem;
+
+	s = ns / 1000000000;
+	rem = ns - s * 1000000000;
+	r->tv_sec = t->tv_sec + s;
+	r->tv_nsec = t->tv_nsec + rem;
+	if (r->tv_nsec >= 1000000000) {
+	     r->tv_sec++;
+	     r->tv_nsec -= 1000000000;
+	}
+}
+
 int main(int argc, char **argv) {
 	const char *devname = "/dev/gpiochip0";
 	struct gpiohandle_data data = { 0 };
-	int tfd, ret, oob_gpio_fd;
+	int tfd, tmfd, ret, oob_gpio_fd;
 	unsigned char gpionum = 27;
 	unsigned char val = 0;
+	struct timespec now;
+	__u64 ticks;
+	struct itimerspec value, ovalue;
 	struct evl_sched_attrs attrs = {
 		.sched_policy = SCHED_FIFO,
 		.sched_priority = 99, /* [1-99] */
@@ -64,24 +86,45 @@ int main(int argc, char **argv) {
 	ret = evl_set_schedattr(tfd, &attrs);
 	if (ret)
 		error(1, errno,	"evl_set_schedattr() failed");
+
+	/* Setup timerfd */
+	tmfd = evl_new_timer(EVL_CLOCK_MONOTONIC);
+	if (tmfd < 0)
+		error(1, errno, "evl_new_timer() failed");
+
+	ret = evl_read_clock(EVL_CLOCK_MONOTONIC, &now);
+	if (ret)
+		error(1, errno,	"evl_read_clock() failed");
 	
+	/* EVL always uses absolute timeouts, add 1000us to the current date */
+	timespec_add_ns(&value.it_value, &now, 1000000ULL);
+	value.it_interval.tv_sec = 0;
+	value.it_interval.tv_nsec = 1000000;
+	ret = evl_set_timer(tmfd, &value, &ovalue);
+	if (ret)
+		error(1, errno,	"evl_set_timer() failed");
+
+		
 	while(1) {
+		/* Wait for the next tick to be notified. */
+		ret = oob_read(tmfd, &ticks, sizeof(ticks));
+		if (ret < 0)
+			error(1, errno,	"oob timer read failed");
+
+		if (ticks > 1) {
+			fprintf(stderr, "timer overrun! %lld ticks late\n",
+			 	ticks - 1);
+			break;
+		}
+
 		data.values[0] = val;
 		ret = oob_ioctl(oob_gpio_fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data);
 		if (ret)
 			error(1, errno,
 			"ioctl(GPIOHANDLE_SET_LINE_VALUES_IOCTL) failed");
 
-		evl_usleep(50);
+		val = !val;
 		
-		/* Start flipping the output pin. */
-		data.values[0] = !val;
-		ret = oob_ioctl(oob_gpio_fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data);
-		if (ret)
-			error(1, errno,
-				"ioctl(GPIOHANDLE_SET_LINE_VALUES_IOCTL) failed");
-
-		evl_usleep(50);
 	}
 	
 	return 0;
